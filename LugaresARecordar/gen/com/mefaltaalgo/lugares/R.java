b5a4de0dfd0354f3061dc26daafaa461342e18fc
Dario Lemos <me@mefaltaalgo.com>
2012-10-08 17:50:17 +0200

    Actualizacion

--- LugaresARecordar/gen/com/mefaltaalgo/lugares/R.java
+++ LugaresARecordar/gen/com/mefaltaalgo/lugares/R.java
@@ -15,7 +15,9 @@ public final class R {
         public static final int ic_launcher=0x7f020001;
     }
     public static final class id {
-        public static final int menu_settings=0x7f070000;
+        public static final int btnLista=0x7f070000;
+        public static final int btnMapa=0x7f070001;
+        public static final int menu_settings=0x7f070002;
     }
     public static final class layout {
         public static final int activity_principal=0x7f030000;
@@ -27,6 +29,8 @@ public final class R {
         public static final int app_name=0x7f040000;
         public static final int hello_world=0x7f040001;
         public static final int menu_settings=0x7f040002;
+        public static final int strBtnLista=0x7f040004;
+        public static final int strBtnMapa=0x7f040005;
         public static final int title_activity_principal=0x7f040003;
     }
     public static final class style {
